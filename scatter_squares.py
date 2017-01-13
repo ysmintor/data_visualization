f3a85f77adaa2df5c08e2cb0000c40b4122d35a1
YorkYu <ysmintor@gmail.com>
2017-01-13 11:31:51 +0800

    cacluating data automatically

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
-plt.scatter(x_values, y_values, s=100)
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, s=40)
 # Set chart title and label axes.
 plt.title("Squre Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
@@ -10,5 +11,8 @@ plt.ylabel("Squre of Value", fontsize=14)
 
 # set size of tick labels.
 plt.tick_params(axis='both', which='major', labelsize=14)
+
+# Set the range for each axis.
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
