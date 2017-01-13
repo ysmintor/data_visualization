dd074ae33ad7f2a0059f8ec57f3396a6940e701b
YorkYu <ysmintor@gmail.com>
2017-01-13 16:38:27 +0800

    Building a World Map

--- world_population.py
+++ world_population.py
@@ -1,4 +1,5 @@
 import json
+from country_codes import get_country_code
 
 # Load the data into a list
 filename = 'population_data.json'
@@ -9,5 +10,9 @@ with open(filename) as f:
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
-        population = pop_dict['Value']
-        print(country_name + ": " + population)
+        population = int(float(pop_dict['Value']))
+        code = get_country_code(country_name)
+        if code:
+            print(code + ": " + str(population))
+        else:
+            print('ERROR - ' + country_name)
