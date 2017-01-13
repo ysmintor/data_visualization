7d5aa5fc54a30f3c719910979a4e1e215ecdffec
YorkYu <ysmintor@gmail.com>
2017-01-13 14:56:54 +0800

    rolling the die

--- rw_visual.py
+++ rw_visual.py
@@ -5,12 +5,14 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active.
 while True:
     # Make a random walk, and plot the points.
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
+    # Set the size of the plotting window.
+    plt.figure(figsize=(10, 6))
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-                edgecolors='none', s=15)
+                edgecolors='none', s=1)
 
     # Emphasize the first and last points.
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
