21f25b055725b0cd5172f8c8810c448c28f01fa3
YorkYu <ysmintor@gmail.com>
2017-01-13 18:04:34 +0800

    finish github repos

--- python_repos.py
+++ python_repos.py
@@ -1,27 +1,36 @@
 import requests
 import pygal
 from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
-# Make an API call and store the response.
+
+# Make an API call, and store the response.
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
 print("Status code:", r.status_code)
 
 # Store API response in a variable.
 response_dict = r.json()
-
-# Process results.
 print("Total repositories:", response_dict['total_count'])
 
 # Explore information about the repositories.
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
 
+    # Some projects lack a description, which causes an error when
+    #  labeling bars. Specify a label if there's no description.
+    description = repo_dict['description']
+    if not description:
+        description = "No description provided."
 
-# Make visualization.
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': description,
+        'xlink': repo_dict['html_url'],
+        }
+    plot_dicts.append(plot_dict)
+
 # Make visualization.
 my_style = LS('#333366', base_style=LCS)
 my_style.title_font_size = 24
@@ -39,5 +48,5 @@ chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
